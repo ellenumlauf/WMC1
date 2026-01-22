6768f9e5efe4e9b619b00700e05236af1bcc9901
ellenumlauf <uml250736@spengergasse.at>
2026-01-22 10:18:46 +0100

    letzte Aenderungen

--- navbar.js
+++ navbar.js
@@ -8,3 +8,10 @@ document.querySelectorAll('.mobile-item').forEach(item => {
         this.classList.toggle('active');  // Toggle für die einzelnen Dropdowns
     });
 });
+
+window.addEventListener('resize', () => {
+    const menu = document.getElementById('mobileMenu');
+    if (window.innerWidth >= 768) {
+        menu.classList.remove('active');
+    }
+});
